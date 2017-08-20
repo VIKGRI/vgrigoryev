eb9a924f9f40494a37fca1db7bf7ed04899f2e7b
VIKGRI <vgrigoryev1983@mail.ru>
2017-08-20 23:15:47 +0300

    task[#4087] Comments added

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+*Класс для вывода приветствия на консоль
+*@author vgrigoryev
+*@since 20.08.2017
+*@version 1
+*/
 public class Calculate {
+	/**
+     *Выводит на консоль приветствие
+     *@param args 
+     */
    public static void main(String[] args) {
       System.out.println("Hello, world!");
    }
