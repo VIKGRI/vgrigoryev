5e922faaedbb53728d333167311c99dfcf98ce3e
VIKGRI <vgrigoryev1983@mail.ru>
2017-08-23 21:10:46 +0300

    passed through checkstyle.xml

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,16 +1,16 @@
 package ru.job4j;
 
 /**
-*Класс для вывода приветствия на консоль
+*Класс для вывода приветствия на консоль.
 *@author vgrigoryev
 *@since 20.08.2017
 *@version 1
 */
 public class Calculate {
 	/**
-     *Выводит на консоль приветствие
-     *@param args 
-     */
+     *Выводит на консоль приветствие.
+	 *@param args - args
+	 */
    public static void main(String[] args) {
       System.out.println("Hello, world!");
    }
