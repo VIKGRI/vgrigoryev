23187e66ce1beddbff5e1d6ee44ca8644c31d981
VIKGRI <vgrigoryev1983@mail.ru>
2017-09-21 17:01:39 +0300

    Implementation for array's iterator for prime numbers and tests of methods.

--- chapter_005/src/main/java/ru/grigoryev/iterators/PrimeIt.java
+++ chapter_005/src/main/java/ru/grigoryev/iterators/PrimeIt.java
@@ -1,48 +1,54 @@
 package ru.grigoryev.iterators;
+
 import java.util.Iterator;
 
 /**
  * Iterator for array of elements of int type which returns prime numbers.
+ *
  * @author vgrigoryev
- * @since 21.09.2017
  * @version 1
+ * @since 21.09.2017
  */
 
-public class PrimeIt  implements  Iterator {
+public class PrimeIt implements Iterator {
     /**
      * Inner array.
      */
-    private  final int[] values;
+    private final int[] values;
     /**
      * First index in the array.
      */
     private int index = 0;
+
     /**
-     *Constructor.
+     * Constructor.
+     *
      * @param values array
      */
-    public PrimeIt(final  int[] values) {
+    public PrimeIt(final int[] values) {
         this.values = values;
     }
 
     /**
      * Method identifies whether the value prime or not.
+     *
      * @param value to check
      * @return true if value is prime and false otherwise
      */
     private boolean isPrime(int value) {
         boolean isPrime = true;
-        int limit = (int)Math.sqrt(value);
-        for(int i = 2; i <= limit; i++) {
-            if(value % i == 0) {
-                isPrime = false;
-                break;
+        if (value > 1) {
+            int limit = (int) Math.sqrt(value);
+            for (int i = 2; i <= limit; i++) {
+                if (value % i == 0) {
+                    isPrime = false;
+                    break;
+                }
             }
-        }
-        if(value == 1) {
+        } else {
             isPrime = false;
         }
-        return  isPrime;
+        return isPrime;
     }
 
     @Override
@@ -56,6 +62,7 @@ public class PrimeIt  implements  Iterator {
         }
         return isNextPrimePresent;
     }
+
     /*
     When there are no prime numbers, then 0 returns.
      */
